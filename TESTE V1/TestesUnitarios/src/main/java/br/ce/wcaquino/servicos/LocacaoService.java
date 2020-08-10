cdbed53fd74f177f9b22922137bb9b28119484cd
joao4018 <joao4018@gmail.com>
2020-08-09 22:46:38 -0300

    Testes V1

---
+++ TESTE V1/TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -0,0 +1,51 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+public class LocacaoService {
+
+    public Locacao alugarFilme(Usuario usuario, ArrayList<Filme> filme) throws FilmeSemEstoqueException, LocadoraException {
+        if (usuario == null) {
+            throw new LocadoraException("Usuario vazio");
+        }
+
+        if (filme == null) {
+            throw new LocadoraException("Filme vazio");
+        }
+
+        for (int i = 0; i < filme.size() ; i++) {
+            if (filme.get(i).getEstoque() == 0)
+                throw new FilmeSemEstoqueException();
+        }
+
+
+
+
+
+        Locacao locacao = new Locacao();
+        locacao.setFilme(filme);
+        locacao.setUsuario(usuario);
+        locacao.setDataLocacao(new Date());
+        filme.forEach(f -> locacao.setValor(f.getPrecoLocacao()));
+
+
+        //Entrega no dia seguinte
+        Date dataEntrega = new Date();
+        dataEntrega = adicionarDias(dataEntrega, 1);
+        locacao.setDataRetorno(dataEntrega);
+
+        //Salvando a locacao...
+        //TODO adicionar método para salvar
+
+        return locacao;
+    }
+}
